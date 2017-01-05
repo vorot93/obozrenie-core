e627c5ffa7629134a7f9f783b2cfe34e9c6cc505
Artem Vorotnikov <artem@vorotnikov.me>
2017-01-05 03:19:36 +0300

    Multiple improvements and fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+#![feature(proc_macro)]
+
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate custom_derive;
+#[macro_use]
+extern crate newtype_derive;
+
+extern crate serde;
+extern crate serde_json;
 extern crate geoip;
 extern crate rgs_models;
-extern crate serde_json;
 
 // So it goes like this:
 // MyGame --- Backend1 - Setting A --- default (determines data type)
@@ -15,18 +25,20 @@ extern crate serde_json;
 
 mod backends;
 mod launch;
+mod models;
 
 use serde_json::Value;
-use std::cmp::*;
 use std::collections::*;
-use std::ops::*;
-use rgs_models::*;
+use std::collections::hash_map::Entry::*;
+use std::sync::Mutex;
+
 use backends::*;
 use launch::*;
-
-// CONFIG
+use models::*;
 
 pub type GameID = String;
+
+// CONFIG
 
 pub struct ConfigEntry {
     pub default: Value,
@@ -51,39 +63,13 @@ pub enum QueryStatus {
     Error,
 }
 
-pub struct ServerEntry(Server);
-
-impl Deref for ServerEntry {
-    type Target = Server;
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Default for QueryStatus {
+    fn default() -> QueryStatus {
+        QueryStatus::Empty
     }
 }
 
-impl PartialEq for ServerEntry {
-    fn eq(&self, other: &Self) -> bool { self.host == other.host }
-}
-
-impl Eq for ServerEntry {}
-
-impl PartialOrd for ServerEntry {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.host.partial_cmp(&other.host)
-    }
-}
-
-impl Ord for ServerEntry {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.host.cmp(&other.host)
-    }
-}
-
-pub type ServerData = BTreeSet<ServerEntry>;
-
-pub type ConfStorage = HashMap<String, Value>;
-
-pub type QueryFunc = fn(GameID, ConfStorage) -> ServerData;
-
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
 pub enum ConfType {
     Launcher,
     Backend,
@@ -94,22 +80,23 @@ impl ConfType {
     pub fn from_string(s: &str) -> Result<ConfType, &'static str> {
         match s {
             "launcher" => Ok(ConfType::Launcher),
-            "backend"  => Ok(ConfType::Backend),
-            "system"   => Ok(ConfType::System),
+            "backend" => Ok(ConfType::Backend),
+            "system" => Ok(ConfType::System),
             _ => Err("Invalid type string"),
         }
     }
     pub fn to_string(&self) -> &str {
         match self {
             &ConfType::Launcher => "launcher",
-            &ConfType::Backend  => "backend",
-            &ConfType::System   => "system",
+            &ConfType::Backend => "backend",
+            &ConfType::System => "system",
         }
     }
 }
 
-pub type Settings = HashMap<String, ConfStorage>;
+pub type Settings = HashMap<ConfType, ConfStorage>;
 
+#[derive(Default)]
 pub struct GameEntry {
     pub status: QueryStatus,
     pub query_func: QueryFunc,
@@ -117,31 +104,80 @@ pub struct GameEntry {
     pub settings: Settings,
 }
 
-fn default_query_func(_: GameID, _: ConfStorage) -> ServerData {
-    return ServerData::new();
-}
-
-impl Default for GameEntry {
-    fn default() -> GameEntry {
-        GameEntry {
-            status: QueryStatus::Empty,
-            query_func: default_query_func,
-            servers: ServerData::new(),
-            settings: Settings::new(),
-        }
-    }
-}
-
+#[derive(Default)]
 pub struct GameTable {
-    pub data: HashMap<GameID, GameEntry>,
+    data: Mutex<HashMap<GameID, GameEntry>>,
+}
+
+fn get_game_entry<'a>(obj: &'a mut HashMap<GameID, GameEntry>, id: &GameID) -> Result<&'a mut GameEntry, Error> {
+    let id = id.clone();
+    obj.get_mut(&id).ok_or(Error::NoSuchGameError(id))
 }
 
 impl GameTable {
     fn new() -> GameTable {
-        GameTable { data: HashMap::new() }
+        GameTable::default()
+    }
+
+    fn exec<T, R>(&self, mut func: T) -> R
+        where T: FnMut(&HashMap<GameID, GameEntry>) -> R
+    {
+        let obj = self.data.lock().unwrap();
+        func(&*obj)
+    }
+
+    fn exec_mut<T, R>(&mut self, mut func: T) -> R
+        where T: FnMut(&mut HashMap<GameID, GameEntry>) -> R
+    {
+        let mut obj = self.data.lock().unwrap();
+        func(&mut *obj)
+    }
+
+    pub fn list_games(&self) -> BTreeSet<GameID> {
+        self.exec(|data| {
+            data.iter().fold(BTreeSet::<GameID>::new(), |mut acc, entry| { acc.insert(entry.0.clone()); acc })
+        })
+    }
+
+    pub fn create_game_entry(&mut self, id: &GameID) -> Result<(), Error> {
+        self.exec_mut(|mut data| {
+            let id = id.clone();
+            match data.entry(id.clone()) {
+                Vacant(entry) => entry.insert(GameEntry::default()),
+                Occupied(_) => {
+                    return Err(Error::GameExistsError(id));
+                }
+            };
+            Ok(())
+        })
+    }
+
+    pub fn remove_game_entry(&mut self, id: &GameID) -> Result<(), Error> {
+        self.exec_mut(|mut data| {
+            let id = id.clone();
+            match data.entry(id.clone()) {
+                Vacant(_) => {
+                    return Err(Error::NoSuchGameError(id));
+                }
+                Occupied(entry) => {
+                    entry.remove_entry();
+                }
+            };
+            Ok(())
+        })
+    }
+    fn get_settings(&mut self, id: &GameID, t: ConfType) -> Result<ConfStorage, Error> {
+        self.exec_mut(|mut data| -> Result<ConfStorage, Error> {
+            let id = id.clone();
+            let game_entry = try!(get_game_entry(&mut data, &id));
+
+            match game_entry.settings.entry(t) {
+                Vacant(e) => Ok(e.insert(ConfStorage::default()).clone()),
+                Occupied(e) => Ok(e.get().clone()),
+            }
+        })
     }
 }
-
 pub struct Core {
     game_table: GameTable,
     geoip: Option<geoip::GeoIp>,
@@ -151,14 +187,28 @@ pub struct Core {
 
 impl Core {
     pub fn new(geoip: Option<geoip::GeoIp>) -> Core {
-        let core = Core {game_table: GameTable::new(), geoip: geoip};
+        let core = Core {
+            game_table: GameTable::new(),
+            geoip: geoip,
+        };
         core
     }
 
-    pub fn refresh_servers(&self, id: GameID) {
+    pub fn refresh_servers(&mut self, id: GameID) {
+        let settings = self.game_table.get_settings(&id, ConfType::Backend);
     }
 
-    pub fn read_game_lists(&self, data: GameList) -> Result<String, String> {
+    pub fn read_game_lists(&mut self, data: GameList) -> Result<String, String> {
         Ok("Success.".to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_game_table() {
+        let obj = GameTable::default();
     }
 }
---
+++ src/models.rs
@@ -0,0 +1,131 @@
+extern crate std;
+extern crate rgs_models;
+extern crate serde_json;
+
+use rgs_models::*;
+use serde_json::Value;
+use std::cmp::*;
+use std::collections::*;
+use std::ops::Deref;
+
+#[derive(Clone, Debug)]
+pub enum Error {
+    NullException(String),
+    NoSuchGameError(String),
+    GameExistsError(String),
+    NotFoundError(String),
+    IOError(String),
+    InvalidUTF8Error(String),
+    DataParseError(String),
+    InvalidConfStorageError(String),
+    InvalidSettingKeyError(String),
+    SettingTypeMismatchError(String),
+    BackendError(String),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::IOError(format!("{}", e))
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(e: std::string::FromUtf8Error) -> Self {
+        Error::InvalidUTF8Error(format!("{}", e))
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Error::DataParseError(format!("{}", e))
+    }
+}
+
+custom_derive!{
+    #[derive(NewtypeDeref, NewtypeDerefMut, Clone, Debug, Default)]
+    pub struct ServerEntry(Server);
+}
+
+impl PartialEq for ServerEntry {
+    fn eq(&self, other: &Self) -> bool {
+        self.host == other.host
+    }
+}
+
+impl Eq for ServerEntry {}
+
+impl PartialOrd for ServerEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.host.partial_cmp(&other.host)
+    }
+}
+
+impl Ord for ServerEntry {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.host.cmp(&other.host)
+    }
+}
+
+pub type ServerData = BTreeSet<ServerEntry>;
+
+pub trait Config<T> {
+    fn get_or_err(&self, k: &str) -> Result<T, Error>;
+}
+
+#[derive(Clone, Default)]
+pub struct ConfStorage(HashMap<String, Value>);
+impl Deref for ConfStorage {
+    type Target = HashMap<String, Value>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Config<Value> for ConfStorage {
+    fn get_or_err(&self, k: &str) -> Result<Value, Error> {
+        let k_str = String::from(k);
+        Ok(try!(self.get(k).ok_or(Error::InvalidSettingKeyError(k_str))).clone())
+    }
+}
+impl Config<String> for ConfStorage {
+    fn get_or_err(&self, k: &str) -> Result<String, Error> {
+        let value: Value = try!(self.get_or_err(k));
+        Ok(String::from(try!(value.as_str().ok_or(Error::SettingTypeMismatchError(k.into())))))
+    }
+}
+impl Config<Vec<String>> for ConfStorage {
+    fn get_or_err(&self, k: &str) -> Result<Vec<String>, Error> {
+        let value: Value = try!(self.get_or_err(k));
+        let arr = try!(value.as_array().ok_or(Error::SettingTypeMismatchError(k.into())));
+        Ok(try!(arr.iter()
+            .map(|ref x| match x.as_str() {
+                Some(v) => Ok(String::from(v)),
+                None => {
+                    Err(Error::SettingTypeMismatchError(format!("Multi-type array detected: {}",
+                                                                k)))
+                }
+            })
+            .collect::<Result<Vec<String>, Error>>()))
+    }
+}
+
+impl Config<bool> for ConfStorage {
+    fn get_or_err(&self, k: &str) -> Result<bool, Error> {
+        let value: Value = try!(self.get_or_err(k));
+        Ok(try!(value.as_bool().ok_or(Error::SettingTypeMismatchError(k.into()))))
+    }
+}
+
+pub struct QueryFunc(Box<Fn(ConfStorage) -> ServerData>);
+impl Deref for QueryFunc {
+    type Target = Box<Fn(ConfStorage) -> ServerData>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Default for QueryFunc {
+    fn default() -> QueryFunc {
+        QueryFunc(Box::new(|_| ServerData::default()))
+    }
+}
